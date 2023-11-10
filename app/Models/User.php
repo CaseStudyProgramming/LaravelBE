926a83f6e9bc0e794e622a4008596b51fb689fa9
CaseStudyProgramming <casestudyprogramming@gmail.com>
2023-11-10 13:42:36 +0700

    setup db(4)

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
@@ -12,4 +13,10 @@ class User extends Model
     protected $keyType = "int";
     protected $timestamps = "true";
     protected $incrementing = "true";
+
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(Contact::class, 'user_id', 'id');
+    }
 }
